091a62e4883356a84cd40d106df308751c191708
Eric Westphal <BonanzaEric@gmail.com>
2019-04-28 00:12:35 +0000

    Working web UI sends parameters to server

--- cmd/websim/main.go
+++ cmd/websim/main.go
@@ -1,31 +1,102 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
-	"path/filepath"
-	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
+type source int // Source is where the measurements come from
 
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	template *template.Template // template represents a single template
+const (
+	manual source = iota // User sends measurements through websocket
+	random // Measurements are made randomly
+	file // Measurements come from a file
+	actual // Measurements come from an actual MPU sensor
+)
+
+type params struct {
+	Source  source `json:"source"`   // Source of the magnetometer data
+	N       int     `json:"n"`       // Number of dimensions
+	N0      float64 `json:"n0"`      // Value of Earth's magnetic field at location
+    NSigma  float64 `json:"nSigma"`  // Initial noise scale for k
+    Epsilon float64 `json:"epsilon"` // Noise scale for measurement and process noise
 }
 
-// ServeHTTP handles the HTTP request.
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.template = template.Must(template.ParseFiles(filepath.Join("res", t.filename)))
-	})
-	if err := t.template.Execute(w, r); err != nil {
-		log.Fatalf("error executing template: %s\n", err.Error())
-	}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename: "index.html"})
+	fs := http.FileServer(http.Dir("www"))
+	http.Handle("/", fs)
+	http.HandleFunc("/websocket", handleConnections)
+	log.Println("Listening for connections on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error upgrading to websocket: %s\n", err)
+		return
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Error closing websocket: %s\n", err.Error())
+		}
+	}()
+	log.Println("A client opened a connection")
+
+	// Handle ping-pong
+	var timeout *time.Timer
+	go func() {
+		pingTime := time.NewTicker(5 * time.Second)
+		for {
+			if err = conn.WriteControl(websocket.PingMessage, []byte("ping"), time.Now().Add(10*time.Second)); err != nil {
+				log.Printf("ws error sending ping: %s\n", err)
+				break
+			}
+			timeout = time.NewTimer(4 * time.Second)
+
+			select {
+			case <-pingTime.C:
+				timeout.Stop()
+			case <-timeout.C:
+				log.Println("ping timeout")
+				pingTime.Stop()
+				if err = conn.Close(); err != nil {
+					log.Printf("Error closing connection: %s\n", err)
+				}
+				break
+			}
+		}
+		log.Println("Stopping ping")
+	}()
+	conn.SetPongHandler(func(appData string) error {
+		timeout.Stop()
+		return nil
+	})
+
+	// Read messages from the client and receive control messages
+	log.Println("Listening for messages from a new client")
+	var p params
+	for {
+		if err = conn.ReadJSON(&p); err != nil {
+			log.Printf("Error reading from websocket: %s\n", err)
+			break
+		}
+		log.Print(p)
+	}
+	log.Println("Closing client")
+}
+
+/*
+if err = conn.WriteJSON(<-myData); err != nil {
+	log.Printf("Error writing to websocket: %s\n", err)
+}
+ */
---
+++ cmd/websim/www/js/app.js
@@ -0,0 +1,77 @@
+var params = {};
+
+vm = new Vue({
+    el: '#app',
+
+    data: {
+        ws: null, // Our websocket
+        source: 0, // Data source selected by the user: manual, random, file, actual
+        n: 1, // Number of dimensions
+        n0: 1.0, // Strength of magnetic field
+        nSigma: 0.1, // Standard deviation of uncertainty of k
+        epsilon: 0.01, // Small noise scale
+        msgContent: '', // A running list of data messages displayed on the screen
+        params: params
+    },
+
+    created: function() {
+        var self = this;
+        this.ws = new WebSocket('ws://' + window.location.host + '/websocket');
+        this.ws.addEventListener('message', handleMessages);
+
+        params = {
+            source: this.source,
+            n: this.n,
+            n0: this.n0,
+            nSigma: this.nSigma,
+            epsilon: this.epsilon
+        };
+    },
+
+    methods: {
+        check_n: function() {
+            if (
+                this.n !== Math.floor(this.n) ||
+                this.n < 1 ||
+                this.n > 3
+            ) { this.n = params.n; }
+        },
+        check_n0: function() {
+            if (this.n0 <= 0) { this.n0 = params.n0; }
+        },
+        check_nSigma: function() {
+            if (this.nSigma <= 0) { this.nSigma = params.nSigma; }
+        },
+        check_epsilon: function() {
+            if (this.epsilon <= 0) { this.epsilon = params.epsilon; }
+        },
+        start: function () {
+            params = {
+                source: parseInt(this.source),
+                n: this.n,
+                n0: this.n0,
+                nSigma: this.nSigma,
+                epsilon: this.epsilon
+            };
+
+            console.log(JSON.stringify(params));
+            this.ws.send(
+                JSON.stringify(params)
+            );
+        }
+    }
+});
+
+function handleMessages(e) {
+    var msg = JSON.parse(e.data);
+    self.msgContent += msg + '<br/>';
+
+    var element = document.getElementById('messages');
+    element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var options = {};
+    var instances = M.FormSelect.init(elems, options);
+});
