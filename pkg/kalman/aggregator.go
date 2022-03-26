67b48022a778e6f662e088bef7b32fa4b7e40768
Eric Westphal <eric@dormouse.com>
2022-03-26 11:13:25 -0400

    gofmt on aggregator*.go

--- pkg/kalman/aggregator.go
+++ pkg/kalman/aggregator.go
@@ -1,6 +1,3 @@
-// Define a data structure that aggregates measurements
-// Averages/counts measurements that are "close"
-// Can quickly apply the current K/L and return the associated N^2
 package kalman
 
 import (
@@ -8,14 +5,14 @@ import (
 )
 
 const (
-	deg = math.Pi/180
+	deg = math.Pi / 180
 )
 
 type MeasureAgg struct {
 	M0, M1, M2 float64 // Averaged measurements near this point
-	MM float64 // M0*M0+M1*M1+M2*M2
-	SS float64 // variance of all measurements relative to mean
-	N  int // number of measurements included at this point
+	MM         float64 // M0*M0+M1*M1+M2*M2
+	SS         float64 // variance of all measurements relative to mean
+	N          int     // number of measurements included at this point
 }
 
 func (a *MeasureAgg) Add(m0, m1, m2 float64) {
@@ -23,13 +20,13 @@ func (a *MeasureAgg) Add(m0, m1, m2 float64) {
 	mOld1 := a.M1
 	mOld2 := a.M2
 	fN := float64(a.N)
-	a.M0 = (fN*mOld0+m0)/(fN+1)
-	a.M1 = (fN*mOld1+m1)/(fN+1)
-	a.M2 = (fN*mOld2+m2)/(fN+1)
-	a.MM = (fN*a.MM+m0*m0+m1*m1+m2*m2)/(fN+1)
-	a.SS = (fN*(a.SS +
-		(mOld0-a.M0)*(mOld0-a.M0) + (mOld1-a.M1)*(mOld1-a.M1) + (mOld2-a.M2)*(mOld2-a.M2)) +
-		(m0-a.M0)*(m0-a.M0) + (m1-a.M1)*(m1-a.M1) + (m2-a.M2)*(m2-a.M2))/(fN+1)
+	a.M0 = (fN*mOld0 + m0) / (fN + 1)
+	a.M1 = (fN*mOld1 + m1) / (fN + 1)
+	a.M2 = (fN*mOld2 + m2) / (fN + 1)
+	a.MM = (fN*a.MM + m0*m0 + m1*m1 + m2*m2) / (fN + 1)
+	a.SS = (fN*(a.SS+
+		(mOld0-a.M0)*(mOld0-a.M0)+(mOld1-a.M1)*(mOld1-a.M1)+(mOld2-a.M2)*(mOld2-a.M2)) +
+		(m0-a.M0)*(m0-a.M0) + (m1-a.M1)*(m1-a.M1) + (m2-a.M2)*(m2-a.M2)) / (fN + 1)
 	a.N += 1
 }
 
@@ -46,16 +43,16 @@ func NewMeasureGrid(nTheta int) (g MeasureGrid) {
 	g.Thetas = make([]float64, nTheta)
 	g.Phis = make([][]float64, nTheta)
 	g.measurements = make([][]MeasureAgg, nTheta)
-	patchSize := (math.Pi/fnTheta)*(math.Pi/fnTheta)
-	for i:=0; i<nTheta; i++ {
-		g.Thetas[i] = math.Pi*(0.5-(float64(i)+0.5)/fnTheta)
+	patchSize := (math.Pi / fnTheta) * (math.Pi / fnTheta)
+	for i := 0; i < nTheta; i++ {
+		g.Thetas[i] = math.Pi * (0.5 - (float64(i)+0.5)/fnTheta)
 		nPhi := int(2*math.Pi*
-			(math.Sin(g.Thetas[i]+0.5*math.Pi/fnTheta)-math.Sin(g.Thetas[i]-0.5*math.Pi/fnTheta)) /
-		patchSize+0.5)
+			(math.Sin(g.Thetas[i]+0.5*math.Pi/fnTheta)-math.Sin(g.Thetas[i]-0.5*math.Pi/fnTheta))/
+			patchSize + 0.5)
 		g.Phis[i] = make([]float64, nPhi)
 		g.measurements[i] = make([]MeasureAgg, nPhi)
-		for j:=0; j<nPhi; j++ {
-			g.Phis[i][j] = 2*math.Pi*(float64(j)+0.5)/float64(nPhi)
+		for j := 0; j < nPhi; j++ {
+			g.Phis[i][j] = 2 * math.Pi * (float64(j) + 0.5) / float64(nPhi)
 		}
 		g.Size += nPhi
 	}
@@ -64,10 +61,10 @@ func NewMeasureGrid(nTheta int) (g MeasureGrid) {
 
 func (g *MeasureGrid) Add(m0, m1, m2 float64) {
 	mm := math.Sqrt(m0*m0 + m1*m1 + m2*m2)
-	theta := math.Asin(m2/mm)
+	theta := math.Asin(m2 / mm)
 	phi := math.Atan2(m1, m0)
-	iTheta := int((0.5-theta/math.Pi)*float64(len(g.Thetas)))
-	iPhi := int((phi+2*math.Pi)/(2*math.Pi)*float64(len(g.Phis[iTheta])))%len(g.Phis[iTheta])
+	iTheta := int((0.5 - theta/math.Pi) * float64(len(g.Thetas)))
+	iPhi := int((phi+2*math.Pi)/(2*math.Pi)*float64(len(g.Phis[iTheta]))) % len(g.Phis[iTheta])
 
 	g.measurements[iTheta][iPhi].Add(m0, m1, m2)
 	g.N += 1
@@ -75,9 +72,9 @@ func (g *MeasureGrid) Add(m0, m1, m2 float64) {
 
 func (g MeasureGrid) Ns() (c [][]int) {
 	c = make([][]int, len(g.Thetas))
-	for i:=0; i<len(g.Thetas); i++ {
+	for i := 0; i < len(g.Thetas); i++ {
 		c[i] = make([]int, len(g.Phis[i]))
-		for j:=0; j<len(g.Phis[i]); j++ {
+		for j := 0; j < len(g.Phis[i]); j++ {
 			c[i][j] = g.measurements[i][j].N
 		}
 	}
@@ -86,11 +83,11 @@ func (g MeasureGrid) Ns() (c [][]int) {
 
 func (g MeasureGrid) Averages() (m [][][]float64) {
 	m = make([][][]float64, len(g.Thetas))
-	for i:=0; i<len(g.Thetas); i++ {
+	for i := 0; i < len(g.Thetas); i++ {
 		m[i] = make([][]float64, len(g.Phis[i]))
-		for j:=0; j<len(g.Phis[i]); j++ {
+		for j := 0; j < len(g.Phis[i]); j++ {
 			m[i][j] = []float64{g.measurements[i][j].M0, g.measurements[i][j].M1, g.measurements[i][j].M2}
-			if g.measurements[i][j].N==0 {
+			if g.measurements[i][j].N == 0 {
 				m[i][j] = []float64{math.NaN(), math.NaN(), math.NaN()}
 			}
 		}
@@ -100,11 +97,11 @@ func (g MeasureGrid) Averages() (m [][][]float64) {
 
 func (g MeasureGrid) StDevs() (s [][]float64) {
 	s = make([][]float64, len(g.Thetas))
-	for i:=0; i<len(g.Thetas); i++ {
+	for i := 0; i < len(g.Thetas); i++ {
 		s[i] = make([]float64, len(g.Phis[i]))
-		for j:=0; j<len(g.Phis[i]); j++ {
+		for j := 0; j < len(g.Phis[i]); j++ {
 			s[i][j] = g.measurements[i][j].SS
-			if g.measurements[i][j].N==0 {
+			if g.measurements[i][j].N == 0 {
 				s[i][j] = math.NaN()
 			}
 		}
@@ -115,11 +112,11 @@ func (g MeasureGrid) StDevs() (s [][]float64) {
 func (g MeasureGrid) CalculatedField(k, l []float64) (f [][][]float64) {
 	m := g.Averages()
 	f = make([][][]float64, len(m))
-	for i:=0; i<len(m); i++ {
+	for i := 0; i < len(m); i++ {
 		f[i] = make([][]float64, len(m[i]))
-		for j:=0; j<len(m[i]); j++ {
-			f[i][j] = []float64{k[0]*(m[i][j][0]-l[0]), k[1]*(m[i][j][1]-l[1]), k[2]*(m[i][j][2]-l[2])}
-			if g.measurements[i][j].N==0 {
+		for j := 0; j < len(m[i]); j++ {
+			f[i][j] = []float64{k[0] * (m[i][j][0] - l[0]), k[1] * (m[i][j][1] - l[1]), k[2] * (m[i][j][2] - l[2])}
+			if g.measurements[i][j].N == 0 {
 				f[i][j] = []float64{math.NaN(), math.NaN(), math.NaN()}
 			}
 		}
@@ -130,9 +127,9 @@ func (g MeasureGrid) CalculatedField(k, l []float64) (f [][][]float64) {
 func (g MeasureGrid) CalculatedFieldStrength2(k, l []float64) (s [][]float64) {
 	m := g.Averages()
 	s = make([][]float64, len(m))
-	for i:=0; i<len(m); i++ {
+	for i := 0; i < len(m); i++ {
 		s[i] = make([]float64, len(m[i]))
-		for j:=0; j<len(m[i]); j++ {
+		for j := 0; j < len(m[i]); j++ {
 			s[i][j] = (k[0]*(m[i][j][0]-l[0]))*(k[0]*(m[i][j][0]-l[0])) +
 				(k[1]*(m[i][j][1]-l[1]))*(k[1]*(m[i][j][1]-l[1])) +
 				(k[2]*(m[i][j][2]-l[2]))*(k[2]*(m[i][j][2]-l[2]))
--- pkg/kalman/aggregator_test.go
+++ pkg/kalman/aggregator_test.go
@@ -10,7 +10,7 @@ import (
 const eps = 1e-6
 
 func testDiff(name string, actual, expected float64, eps float64, t *testing.T) {
-	if actual - expected > -eps && actual - expected < eps {
+	if actual-expected > -eps && actual-expected < eps {
 		t.Logf("%s correct: expected %8.4f, got %8.4f", name, expected, actual)
 		return
 	}
@@ -18,19 +18,19 @@ func testDiff(name string, actual, expected float64, eps float64, t *testing.T) 
 }
 
 func TestPointAggregation(t *testing.T) {
-	ms := [][]float64{[]float64{-1,2.9,-11.5}, []float64{0,3,-12}, []float64{1,3.1,-11}}
+	ms := [][]float64{{-1, 2.9, -11.5}, {0, 3, -12}, {1, 3.1, -11}}
 	mavgs := []float64{0, 3, -11.5}
 
 	a := MeasureAgg{}
 	mm := 0.0
 	ss := 0.0
-	for i:=0; i<len(ms); i++ {
+	for i := 0; i < len(ms); i++ {
 		a.Add(ms[i][0], ms[i][1], ms[i][2])
-		mm += ms[i][0]*ms[i][0]+ms[i][1]*ms[i][1]+ms[i][2]*ms[i][2]
-		ss += ms[i][0]*ms[i][0]+ms[i][1]*ms[i][1]+ms[i][2]*ms[i][2]
+		mm += ms[i][0]*ms[i][0] + ms[i][1]*ms[i][1] + ms[i][2]*ms[i][2]
+		ss += ms[i][0]*ms[i][0] + ms[i][1]*ms[i][1] + ms[i][2]*ms[i][2]
 	}
-	mm = mm/float64(len(ms))
-	ss = ss/float64(len(ms))-(mavgs[0]*mavgs[0]+mavgs[1]*mavgs[1]+mavgs[2]*mavgs[2])
+	mm = mm / float64(len(ms))
+	ss = ss/float64(len(ms)) - (mavgs[0]*mavgs[0] + mavgs[1]*mavgs[1] + mavgs[2]*mavgs[2])
 	testDiff("N", float64(a.N), float64(len(ms)), eps, t)
 	testDiff("M0", a.M0, mavgs[0], eps, t)
 	testDiff("M1", a.M1, mavgs[1], eps, t)
@@ -48,21 +48,21 @@ func TestGridShape(t *testing.T) {
 	ns := []int{6}
 	for _, nTheta := range ns {
 		g := NewMeasureGrid(nTheta)
-		if len(g.Thetas)!=nTheta {
+		if len(g.Thetas) != nTheta {
 			t.Errorf("Theta lengths don't match: %d should be %d", len(g.Thetas), nTheta)
 		}
 		for i, theta := range g.Thetas {
-			if i>1 {
+			if i > 1 {
 				testDiff("dTheta", theta-theta0, dTheta, eps, t)
 			}
 			for j, phi := range g.Phis[i] {
-				if j>1 {
+				if j > 1 {
 					testDiff("dPhi", phi-phi0, dPhi, eps, t)
 				}
-				dPhi = phi-phi0
+				dPhi = phi - phi0
 				phi0 = phi
 			}
-			dTheta = theta-theta0
+			dTheta = theta - theta0
 			theta0 = theta
 			size += len(g.Phis[i])
 		}
@@ -83,33 +83,33 @@ func TestGridAreas(t *testing.T) {
 	for _, nTheta := range ns {
 		g := NewMeasureGrid(nTheta)
 		nPatches = 0
-		for i:=0; i<len(g.Phis); i++ {
+		for i := 0; i < len(g.Phis); i++ {
 			nPatches += len(g.Phis[i])
 		}
-		patchSize := 4*math.Pi/float64(nPatches)
+		patchSize := 4 * math.Pi / float64(nPatches)
 
 		for i, theta := range g.Thetas {
-			if i==0 {
-				theta1 = math.Pi/2
+			if i == 0 {
+				theta1 = math.Pi / 2
 			} else {
-				theta1 = 0.5*theta+0.5*g.Thetas[i-1]
+				theta1 = 0.5*theta + 0.5*g.Thetas[i-1]
 			}
-			if i==nTheta-1 {
-				theta0 = -math.Pi/2
+			if i == nTheta-1 {
+				theta0 = -math.Pi / 2
 			} else {
-				theta0 = 0.5*theta+0.5*g.Thetas[i+1]
+				theta0 = 0.5*theta + 0.5*g.Thetas[i+1]
 			}
 
 			for j, phi := range g.Phis[i] {
-				if j==0 {
-					phi0 = 0.5*phi+0.5*(g.Phis[i][len(g.Phis[i])-1]-2*math.Pi)
+				if j == 0 {
+					phi0 = 0.5*phi + 0.5*(g.Phis[i][len(g.Phis[i])-1]-2*math.Pi)
 				} else {
-					phi0 = 0.5*phi+0.5*g.Phis[i][j-1]
+					phi0 = 0.5*phi + 0.5*g.Phis[i][j-1]
 				}
-				if j==len(g.Phis[i])-1 {
-					phi1 = 0.5*phi+0.5*(g.Phis[i][0]+2*math.Pi)
+				if j == len(g.Phis[i])-1 {
+					phi1 = 0.5*phi + 0.5*(g.Phis[i][0]+2*math.Pi)
 				} else {
-					phi1 = 0.5*phi+0.5*g.Phis[i][j+1]
+					phi1 = 0.5*phi + 0.5*g.Phis[i][j+1]
 				}
 
 				testDiff(
@@ -124,56 +124,56 @@ func TestGridAreas(t *testing.T) {
 
 func TestGridNsRandom(t *testing.T) {
 	r := rand.New(rand.NewSource(99))
-	for j:=0; j<1; j++ {
+	for j := 0; j < 1; j++ {
 		nTheta := r.Intn(100)
 		g := NewMeasureGrid(nTheta)
 		ns := make([][]int, nTheta)
-		for i:=0; i<nTheta; i++ {
+		for i := 0; i < nTheta; i++ {
 			ns[i] = make([]int, len(g.Phis[i]))
 		}
-		for i:=0; i<r.Intn(1000); i++ {
+		for i := 0; i < r.Intn(1000); i++ {
 			ti := r.Intn(len(g.Thetas))
 			si := r.Intn(len(g.Phis[ti]))
-			m0 := math.Cos(g.Thetas[ti])*math.Cos(g.Phis[ti][si])
-			m1 := math.Cos(g.Thetas[ti])*math.Sin(g.Phis[ti][si])
+			m0 := math.Cos(g.Thetas[ti]) * math.Cos(g.Phis[ti][si])
+			m1 := math.Cos(g.Thetas[ti]) * math.Sin(g.Phis[ti][si])
 			m2 := math.Sin(g.Thetas[ti])
 			g.Add(m0, m1, m2)
 			ns[ti][si] += 1
 		}
 		c := g.Ns()
 		for i, d := range c {
-			for j, _ := range d {
+			for j := range d {
 				testDiff(fmt.Sprintf("(%d,%d)", i, j), float64(c[i][j]), float64(ns[i][j]), 0.5, t)
 			}
 		}
 	}
 }
 
-func makeTestGrid() (g MeasureGrid, ns [][]int, avgs [][][]float64, stds [][]float64){
-	ms := [][]float64{[]float64{11000,18000,9500},[]float64{10200,17100,8001}}
-	dTheta := math.Pi/3
+func makeTestGrid() (g MeasureGrid, ns [][]int, avgs [][][]float64, stds [][]float64) {
+	ms := [][]float64{{11000, 18000, 9500}, {10200, 17100, 8001}}
+	dTheta := math.Pi / 3
 
 	g = NewMeasureGrid(len(ns))
-	ns = [][]int{[]int{12,8,16},[]int{22,6,14}}
+	ns = [][]int{{12, 8, 16}, {22, 6, 14}}
 	avgs = make([][][]float64, len(ns))
 	stds = make([][]float64, len(ns))
 	var m0, m1, m2, jj, s float64
-	for i:=0; i<2; i++ {
+	for i := 0; i < 2; i++ {
 		ns[i] = make([]int, len(ns[i]))
 		avgs[i] = make([][]float64, len(ns[i]))
 		stds[i] = make([]float64, len(ns[i]))
-		for j:=0; j<3; j++ {
-			jj = float64(2*j+1)
-			m0 = ms[i][j]*math.Cos(jj*dTheta)*math.Cos(dTheta)
-			m1 = ms[i][j]*math.Cos(jj*dTheta)*math.Cos(dTheta)
-			m2 = ms[i][j]*math.Cos(jj*dTheta)*math.Cos(dTheta)
+		for j := 0; j < 3; j++ {
+			jj = float64(2*j + 1)
+			m0 = ms[i][j] * math.Cos(jj*dTheta) * math.Cos(dTheta)
+			m1 = ms[i][j] * math.Cos(jj*dTheta) * math.Cos(dTheta)
+			m2 = ms[i][j] * math.Cos(jj*dTheta) * math.Cos(dTheta)
 			s = 1
-			for k:=0; k<ns[i][j]; k++ {
+			for k := 0; k < ns[i][j]; k++ {
 				g.Add((1+0.1*s)*m0, (1+0.1*s)*m1, (1+0.1*s)*m2)
 				s = -s
 			}
 			avgs[i][j] = []float64{m0, m1, m2}
-			stds[i][j] = 0.1*ms[i][j]
+			stds[i][j] = 0.1 * ms[i][j]
 		}
 	}
 
@@ -217,9 +217,9 @@ func TestGridCalculatedFieldStrength2s(t *testing.T) {
 	var n0, n1, n2 float64
 	for i, theta := range g.Thetas {
 		for _, phi := range g.Phis[i] {
-			n0 = nn*math.Cos(theta)*math.Cos(phi)
-			n1 = nn*math.Cos(theta)*math.Sin(phi)
-			n2 = nn*math.Sin(theta)
+			n0 = nn * math.Cos(theta) * math.Cos(phi)
+			n1 = nn * math.Cos(theta) * math.Sin(phi)
+			n2 = nn * math.Sin(theta)
 			g.Add(n0/k[0]+l[0], n1/k[1]+l[1], n2/k[2]+l[2])
 		}
 	}
@@ -240,16 +240,16 @@ func TestGridCalculatedFieldStrengths(t *testing.T) {
 		n[i] = make([][]float64, len(g.Phis[i]))
 		for j, phi := range g.Phis[i] {
 			n[i][j] = []float64{
-				nn*math.Cos(theta)*math.Cos(phi),
-				nn*math.Cos(theta)*math.Sin(phi),
-				nn*math.Sin(theta),
+				nn * math.Cos(theta) * math.Cos(phi),
+				nn * math.Cos(theta) * math.Sin(phi),
+				nn * math.Sin(theta),
 			}
 			g.Add(n[i][j][0]/k[0]+l[0], n[i][j][1]/k[1]+l[1], n[i][j][2]/k[2]+l[2])
 		}
 	}
 	for i, e := range g.CalculatedField(k, l) {
 		for j, f := range e {
-			for l:=0; l<3; l++ {
+			for l := 0; l < 3; l++ {
 				testDiff(fmt.Sprintf("(%d,%d) Field%d", i, j, l), f[l], n[i][j][l], eps, t)
 			}
 		}
